b483162c3848ac9fa40cf6d77e462d6835a89a8a
treinamento <treinamento@MW7FABE8F1VHV3.dir.svc.accenture.com>
2016-05-25 17:33:24 -0300

    inscricao

--- lancerJob/server.js
+++ lancerJob/server.js
@@ -11,29 +11,37 @@ var options = {
 }
 mongoose.connect('mongodb://localhost/LancerJobs', options);
 
-//Schemas IMPORT
-var ProjetoSchema = require('./schema/projetoShema.js').ProjetoSchema;
-var projetoSchemaInstance = new ProjetoSchema(mongoose);
-
-<<<<<<< HEAD
+//Schemas IMPORT Everton
+//var ProjetoSchema = require('./schema/projetoShema.js').ProjetoSchema;
+//var projetoSchemaInstance = new ProjetoSchema(mongoose);
+// appSchema
+var AppSchema = require('./schema.js').AppSchema;
+var appSchemaInstance = new AppSchema(mongoose);
+//<<<<<<< HEAD
 var CadastroService = require('./cadastroService.js').CadastroService;
 var cadastroServiceInstance = new CadastroService(mongoose, appSchemaInstance);
-=======
+//=======
 
 //Services IMPORT
-var AvaliacaoService = require('./services/avaliacaoService.js').AvaliacaoService;
-var avaliacaoServiceInstance = new AvaliacaoService(mongoose, projetoSchemaInstance);
-var PerfilService = require('./services/perfilService.js').PerfilService;
-var perfilServiceInstance = new PerfilService(mongoose, projetoSchemaInstance);
->>>>>>> branch 'master' of https://github.com/veveton/lancerjobs.git
+//var AvaliacaoService = require('./services/avaliacaoService.js').AvaliacaoService;
+//var avaliacaoServiceInstance = new AvaliacaoService(mongoose, appSchemaInstance);
 
-<<<<<<< HEAD
+//var PerfilService = require('./services/perfilService.js').PerfilService;
+//var perfilServiceInstance = new PerfilService(mongoose, appSchemaInstance);
+//>>>>>>> branch 'master' of https://github.com/veveton/lancerjobs.git
+
+//<<<<<<< HEAD
 var ProjetoService = require('./projetoService.js').ProjetoService;
 var projetoServiceInstance = new ProjetoService(mongoose, appSchemaInstance);
+
+var InscricaoService = require('./inscricaoService.js').InscricaoService;
+var inscricaoServiceInstance = new InscricaoService(mongoose, appSchemaInstance);
 //----------------CadastroService---- Ini-----------
-app.put('/editarCadastro', function (req, res) {
-	cadastroServiceInstance.editarCadastro(req.body, function(response){
-=======
+
+//app.put('/editarCadastro', function (req, res) {
+//	cadastroServiceInstance.editarCadastro(req.body, function(response){
+		
+//=======
 //servicos
 app.post('/salvaAvaliacaor', function (req, res) {
 	avaliacaoServiceInstance.salvarAvaliacao(req.body, function(response){
@@ -42,16 +50,16 @@ app.post('/salvaAvaliacaor', function (req, res) {
 		res.send(err);
 	});
 });
-
+/*
 app.post('/salvaPerfil', function (req, res) {
 	perfilServiceInstance.salvarPerfil(req.body, function(response){
->>>>>>> branch 'master' of https://github.com/veveton/lancerjobs.git
+//>>>>>>> branch 'master' of https://github.com/veveton/lancerjobs.git
 		res.send(response);
 	}, function(err){
 		res.send(err);
 	});
 });
-
+*/
 app.get("/listarCadastro", function(req,res){
 	
 	cadastroServiceInstance.listarCadastroService(req.body, function(response){
@@ -75,7 +83,7 @@ app.post('/addCadastro', function (req, res) {
 
 
 
-//----------------CadastroService---- Ini-----------
+//----------------ProjetoService---- Ini-----------
 app.delete('/removerProjeto/:id', function (req, res) {
 	
 	projetoServiceInstance.removerProjetoService(req.params.id, function(response){
@@ -102,11 +110,29 @@ app.post('/addProjeto', function (req, res) {
 	});
 });
 
-//----------------CadastroService---- fim-----------
+//----------------ProjetoService---- fim-----------
+//----------------inscricaoService---- Ini-----------
+
+
+app.get("/listarInscricao", function(req,res){
+	
+	inscricaoServiceInstance.listarInscricaoService(req.body, function(response){
+		res.send(response);
+	}, function(err){
+		res.send(err);
+	});
+});
+
+app.post('/addInscricao', function (req, res) {
+	inscricaoServiceInstance.addInscricaoService(req.body, function(response){
+		res.send(response);
+	}, function(err){
+		res.send(err);
+	});
+});
+
+
+//----------------inscricaoService---- fim-----------
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
-<<<<<<< HEAD
 });
-=======
-})
->>>>>>> branch 'master' of https://github.com/veveton/lancerjobs.git
