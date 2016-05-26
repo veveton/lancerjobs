1483560e18c5f5418e735343df4670823847d9a4
veveton <veveton@gmail.com>
2016-05-26 20:49:24 -0300

    Add files via upload
    Adição Controller Tela inicial

---
+++ lancerJob/public/js/indexController.js
@@ -0,0 +1,35 @@
+angular.module("angularApp").controller("inicioController", function($scope, $http) {
+	$scope.email="";
+	$scope.password="";
+	
+	
+	$scope.validarLogin = function(){//ADICIONA CONTATO
+		
+			
+			var usuario = {email:$scope.email,senha:$scope.password};//PEGA O VALOR DO CAMPO
+			$http.post("/sessaoAdicionar",usuario, {
+				headers:{ 'Content-Type' : 'application/json' }
+			})
+			.then(
+				function(response){//CASO TRUE LIMPA E LISTA
+					
+					
+					$scope.email="";
+					$scope.password="";
+					alert("Loguin OK com Sucesso!")
+			
+				},
+				function(response){//CASO DE ERRO
+					alert(response.data);
+				}
+			);
+			
+		}
+		
+	
+		
+		
+		
+		
+		
+});
