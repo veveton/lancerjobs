0de90e143680f422029cbf767aaec306e3a13601
veveton <veveton@gmail.com>
2016-05-26 20:47:53 -0300

    Update userService.js
    UserService OK

--- lancerJob/services/userService.js
+++ lancerJob/services/userService.js
@@ -5,20 +5,37 @@ function UserService(mongoose, appSchema){
 	this.addUserService = function(p_cadastro, successCallback, errorCallback){
 		var cadastroSave = new Cadastro(p_cadastro);
 		cadastroSave.save(function (err, data){
-			if (err){ errorCallback(err);}
+			if (err) errorCallback(err);
 			
-			else {successCallback(data);}
+			else successCallback(data);
 			});
 	}
+	
 	this.listarUserService = function(p_cadastro,successCallback, errorCallback){		
-				Cadastro.find({}, function (err, data) {
-					if (err){ errorCallback(err);}
+		
+		var query = {};		
+		Cadastro.find(query).exec (function (err, data) {
+					if (err) errorCallback(err);
 					
-					else{successCallback(data);}
+					else successCallback(data);
 
 				});	
 	}
 	
-};			
+this.loguinService = function(p_cadastro,successCallback, errorCallback){	
+	
+		console.log(p_cadastro);
+		var query = {email:p_cadastro.email,senha:p_cadastro.senha};		
+		Cadastro.find(query).exec (function (err, data) {
+					if (err) errorCallback(err);
+					
+					else successCallback(data);
+						
+				});	
+	}
+	
+	
+}
+			
 	
 module.exports.UserService = UserService;
