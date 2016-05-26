2f7bfb9fd9a96548fcdb644d27377909dadbab27
treinamento <treinamento@MW7FABE8F1VHV3.dir.svc.accenture.com>
2016-05-26 09:56:41 -0300

    Mudança dos schemas, feito em equipe.

--- lancerJob/schema/schema.js
+++ lancerJob/schema/schema.js
@@ -1,31 +1,31 @@
 function AppSchema(mongoose){
 	var Schema = mongoose.Schema;
-
-	this.cadastroSchema = new Schema({
-		nome: String,
-		cpf: String,
-		email:String,
-		senha: String,
+	
+	this.avaliacaoSchema = new Schema({
+		
+		nota: Number,
+		descricao: String,
+		replica: String,
 		createdOn: {type: Date, default: Date.now}
 	});
 	
 	this.perfilSchema = new Schema({
-		iduser: String,
 		nome: String,
 		especialidade: String,
-		ranking: String,
+		ranking: Number,
 		projConcluido: String,
 		dataRegistro: Date,
 		resumoProfissional: String,
 		perfil: String,
 		habilidades: String,
+		avaliacao:[this.avaliacaoSchema],
 		areaInteresse: String,
-		
+
 	});//FALTA O CAMPO FOTO
-	
-	
+
+
 	this.projetoSchema = new Schema({
-		idcadastro: String,
+
 		nomeproj: String,
 		horas:String,
 		prazofinal: String,
@@ -35,26 +35,36 @@ function AppSchema(mongoose){
 		prioridade: String,
 		descricao: String,
 		valor: String,
+		aprovadoStatus: Boolean,
+		inscritos:[this.perfilSchema],
 		proposta: String,
 		createdOn: {type: Date, default: Date.now}
 	});
-	
-	this.inscricaoSchema = new Schema({
-		idproj: String,
-		idcadastro: String,
-		status: Boolean,
+
+
+	this.userSchema = new Schema({ // nome do schema mudou
+		nome: String,
+		cpf: String,
+		email:String,
+		senha: String,
+		perfil: this.perfilSchema,
+		projetosAnuciado:[this.projetoSchema],
+		projetosConcluidos:[this.projetoSchema],
 		createdOn: {type: Date, default: Date.now}
+
 	});
+
+
+
 	
-	this.avaliacaoSchema = new Schema({
-		idProj: String,
-		idPerfil: String,
-		nota: String,
-		descricao: String,
-		replica: String,
-		createdOn: {type: Date, default: Date.now}
-	});
 }
 
 
+/*
+this.inscricaoSchema = new Schema({
+	idproj: String,
+	idcadastro: String,
+	status: Boolean,
+	createdOn: {type: Date, default: Date.now}
+});*/
 module.exports.AppSchema = AppSchema;
