002923c21875c48871e8d5377d3096ac6eb7a89b
treinamento <treinamento@MW7FABE8F1VHV3.dir.svc.accenture.com>
2016-05-26 09:16:39 -0300

    projeto

--- lancerJob/public/js/projetoController.js
+++ lancerJob/public/js/projetoController.js
@@ -8,11 +8,11 @@ angular.module("angularApp").controller("projetoController",function($scope,$htt
 	$scope.prioridade = "";
 	$scope.descricao = "";
 	$scope.valor = "";
+	$scope.nomeUser= "";
 
 
 
-
-	$scope.registrar = function(){//ADICIONA PROJETO
+$scope.registrar = function(){//ADICIONA PROJETO
 
 
 		var usuario = {nomeproj:$scope.nomeproj,horas:$scope.horas,prazofinal:$scope.prazofinal,categoria:$scope.categoria,requisitos:$scope.requisitos,nivel:$scope.nivel,prioridade:$scope.prioridade,descricao:$scope.descricao,valor:$scope.valor};//PEGA O VALOR DO CAMPO
@@ -42,5 +42,52 @@ angular.module("angularApp").controller("projetoController",function($scope,$htt
 		/**/
 
 	};
+	
+$scope.listarProjeto = function(){//ADICIONA PROJETO
 
+
+	$http({
+		method:'GET',
+		url:'/listarProjeto'
+	})
+	.then(function successCallback(response) {
+		if($scope.listaContato=response.data==""){
+			alert("Nenhum projeto cadastrado!");
+		
+	}else{$scope.listaContato=response.data;}
+		
+	}, function errorCallback(response)	{
+		alert(response.data);
+	});	
+	
+	};
+$scope.listarUser = function(){
+										//fazer chamada user
+	$http({
+		method:'GET',
+		url:'/'
+	})
+	.then(function successCallback(response) {
+		$scope.listaContato=response.data;
+	}, function errorCallback(response)	{
+		alert(response.data);
+	});
+	
+}
+$scope.listarUser = function();
+
+$scope.removerProjeto = function(id){
+		$http.delete("/removerProjeto/"+id) //eclipse nao reconhece delete
+		.then(
+			function(response){
+				$scope.carregarListaUsuarios();
+			},
+			function(response){
+				alert(response.data);
+			}
+		);
+		
+		}
+
+	
 });
