de20502344cced14aa6b0fe091e552e0568c6f31
treinamento <treinamento@MW7P0F99R1LTPD.dir.svc.accenture.com>
2016-05-27 15:33:38 -0300

    Registro

--- lancerJob/public/js/registroController.js
+++ lancerJob/public/js/registroController.js
@@ -4,40 +4,40 @@ angular.module("angularApp").controller("registroController",function($scope,$ht
 	$scope.email = "";
 	$scope.senha = "";
 	$scope.confSenha = "";
-	
-	
-	
-	$scope.registrar = function(){//ADICIONA CONTATO
-		if($scope.senha == $scope.confSenha){
-			
-			var usuario = {nome:$scope.nome,cpfCnpj:$scope.cpfCnpj,email:$scope.email,senha:$scope.senha};//PEGA O VALOR DO CAMPO
-			$http.post("/addCadastro",usuario, {
-				headers:{ 'Content-Type' : 'application/json' }
-			})
-			.then(
-				function(response){//CASO TRUE LIMPA E LISTA
-					
-					$scope.nome = "";
-					$scope.cpfCnpj = "";
-					$scope.email = "";
-					$scope.senha = "";
-					$scope.confSenha = "";
-					
-					alert("Registrado com Sucesso!")
-			
-				},
-				function(response){//CASO DE ERRO
-					alert(response.data);
-				}
-			);
-			
-		}
-		else{
-			alert("Senhas Não Coincidem!")
-		}
 		
-		/**/
+	$scope.registrar = function() {	//ADICIONA CONTATO
 		
+		// Validação dos campos do formulário
+		
+		if($scope.nome == "") alert("O campo NOME é obrigatório!");
+		else if($scope.cpfCnpj == "") alert("O campo CPF OU CNPJ é obrigatório!");
+		else if($scope.cpfCnpj == "") alert("O campo CPF OU CNPJ é obrigatório!");
+		else if($scope.email == "") alert("O campo E-MAIL é obrigatório!");
+		else if($scope.senha == "") alert("O campo SENHA é obrigatório!");
+		else if($scope.confSenha == "") alert("O campo CONFIRMAR SENHA é obrigatório!");
+		else {
+			if($scope.senha == $scope.confSenha) {
+				
+				var usuario = {nome:$scope.nome,cpfCnpj:$scope.cpfCnpj,email:$scope.email,senha:$scope.senha}; //PEGA O VALOR DO CAMPO
+				$http.post("/addCadastro", usuario, {
+					headers:{ 'Content-Type' : 'application/json' }
+				})
+				.then(
+					function(response){	//CASO TRUE LIMPA E LISTA
+						alert("Registrado com Sucesso!");
+						
+						$scope.nome = "";
+						$scope.cpfCnpj = "";
+						$scope.email = "";
+						$scope.senha = "";
+						$scope.confSenha = "";
+					},
+					function(response){	//CASO DE ERRO
+						alert(response.data);
+					});
+			} // fim IF
+			
+			else alert("Senhas Não Coincidem!");
+		}		
 	};
-	
 });
