0048175850f446b9651ae47e96912dc9e3437e8e
treinamento <treinamento@MW7P0F99R1LTPD.dir.svc.accenture.com>
2016-05-27 14:17:44 -0300

    DELL

--- lancerJob/public/js/angularController.js
+++
@@ -1,9 +0,0 @@
-angular.module("angularApp").controller("angularController", function($scope, $http) {
-	function RatingController() {
-	    this.rating = 4;
-	    this.isReadonly = true;
-	    this.rateFunction = function(rating) {
-	      console.log('Rating selected: ' + rating);
-	    };
-	 }
-});
