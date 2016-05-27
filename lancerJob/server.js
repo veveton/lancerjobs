01ab66052ed4e631318cb429c6d1adb2812d08fb
treinamento <treinamento@MW7FABE8F1VHV3.dir.svc.accenture.com>
2016-05-27 09:31:21 -0300

    projeto

--- lancerJob/server.js
+++ lancerJob/server.js
@@ -138,7 +138,7 @@ app.post('/salvaPerfil', function (req, res) {
 	});
 });
 
-app.get("/listarPerfil", function(req,res){
+app.get('/listarPerfil', function(req,res){
 	
 	perfilServiceInstance.listarPerfilService(req.body, function(response){
 		res.send(response);
@@ -187,7 +187,7 @@ app.get("/listarProjeto", function(req,res){
 });
 
 app.put('/addProjeto/:id', function (req, res) {
-	projetoServiceInstance.addProjetoService(req.body, function(response){
+	projetoServiceInstance.addProjetoService(req, function(response){
 		res.send(response);
 	}, function(err){
 		res.send(err);
